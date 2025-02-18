8e4270817b7c44ad7e42976128d1d32b94606313
CyanidePineapple <joshuabeecher2003@gmail.com>
2025-02-18 13:56:55 -0600

    Completed backend server tests

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+  })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
