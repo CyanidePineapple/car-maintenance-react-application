668f89158399a6fccf24db329c0645c1e04aee68
CyanidePineapple <joshuabeecher2003@gmail.com>
2025-02-20 16:56:18 -0600

    backup before adding new files

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
 const routes = require('./routes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
-app.get('/', (req, res) => {
-    res.send('Server is running!');
-  })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log('Server running on port 5000'));
--- backend/models.js
+++ backend/models.js
@@ -17,4 +17,3 @@ const createTables = async () => {
     `);
 };
 createTables();
-createTables();
